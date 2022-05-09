3818994dbb3238eb67f8ece21c9378de8c33fe6d
VisaChan <91771783+VisaChan@users.noreply.github.com>
2022-05-09 15:08:40 -0500

    trying to fail a test

--- app/src/androidTest/java/com/example/apitest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/apitest/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.apitest", appContext.getPackageName());
+        assertEquals("com.example.apitest1", appContext.getPackageName());
     }
 }
